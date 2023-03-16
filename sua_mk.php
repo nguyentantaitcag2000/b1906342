23ab5b9e39faaee206189eea865d7b2982570e16
nguyentantaitcag2000 <66154702+nguyentantaitcag2000@users.noreply.github.com>
2023-03-16 17:31:45 +0700

    Add files via upload

---
+++ sua_mk.php
@@ -0,0 +1,15 @@
+<form action="sua.php" method="post">
+  <div>
+    <label for="currentpassword">Mật khẩu hiện tại:</label>
+    <input type="password" id="currentpassword" name="currentpassword">
+  </div>
+  <div>
+    <label for="newpassword">Mật khẩu mới:</label>
+    <input type="password" id="newpassword" name="newpassword">
+  </div>
+  <div>
+    <label for="confirmpassword">Xác nhận mật khẩu mới:</label>
+    <input type="password" id="confirmpassword" name="confirmpassword">
+  </div>
+  <input type="submit" value="Xác nhận">
+</form>
